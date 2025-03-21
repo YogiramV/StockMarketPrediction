74391984878928b458cd331e1445cfc5336f66ac
YogiramV <yogiramkv@gmail.com>
2025-03-21 14:27:17 +0530

    Update requirements.txt

--- data_fetcher.py
+++ data_fetcher.py
@@ -1,36 +1,3 @@
-# import yfinance as yf
-# import pandas as pd
-
-
-# class DataFetcher:
-#     stock_data = None
-#     symbol = None
-
-#     def __init__(self, symbol, start_date, end_date):
-#         self.symbol = symbol
-#         # Download stock data from Yahoo Finance
-#         self.stock_data = yf.download(symbol, start=start_date, end=end_date)
-
-#         # Flatten the MultiIndex columns
-#         self.stock_data.columns = [
-#             f'{col[0]}_{col[1]}' for col in self.stock_data.columns]
-
-#         # Reset the index so that 'Date' becomes a column
-#         self.stock_data.reset_index(inplace=True)
-
-#         # Ensure 'Date' column is of type datetime (if it's not already)
-#         self.stock_data['Date'] = pd.to_datetime(self.stock_data['Date'])
-
-#         # Set the Date column as the index if it's not already
-#         self.stock_data.set_index('Date', inplace=True)
-
-#     def getStockData(self):
-#         return self.stock_data
-
-#     def getCloseData(self):
-#         # Extract only the 'Close' price for the given stock (e.g., 'AAPL')
-#         return self.stock_data[['Date', 'Close_'+symbol]]
-
 import yfinance as yf
 import pandas as pd
 from datetime import datetime, timedelta
